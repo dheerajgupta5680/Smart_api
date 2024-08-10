dcb064af6f7272c4893d7de2a251101156585a39
dheeraj <dheerajgupta5680@gmail.com>
2024-08-10 13:10:24 +0530

    login

--- newproject/articles/models.py
+++ newproject/articles/models.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django_cryptography.fields import encrypt
 
 class CustomUser(AbstractUser):
     # Your custom fields here
-    pass
+    client_id = models.CharField(max_length=255, null=False, blank=False)
+    totp = encrypt(models.CharField(max_length=255, null=False, blank=False))  # Encrypt TOTP as it's sensitive
+    pin = encrypt(models.CharField(max_length=255, null=False, blank=False))  # Encrypt TOTP as it's sensitive
+    
--- newproject/articles/serializers.py
+++ newproject/articles/serializers.py
@@ -3,16 +3,23 @@ from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    totp = serializers.CharField(write_only=True, required=True)
+    pin = serializers.CharField(write_only=True, required=True)
+    clientID = serializers.CharField(required=True)
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password', 'clientID', 'totp', 'pin')
         
     def create(self, validated_data):
         user = CustomUser(
-            username=validated_data['username'],
-            email=validated_data['email']
+            username = validated_data['username'],
+            email = validated_data['email'],
+            clientID = validated_data['clientID'],
+            totp = validated_data['totp'],
+            pin = validated_data['pin']
         )
         user.set_password(validated_data['password'])  # Hashes the password
         user.save()
         return user
+    
